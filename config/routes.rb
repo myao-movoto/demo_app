278ed464160be03bc35fd3010c82318bc7a7a75b
MY <myao@movoto.com>
2011-11-03 13:11:54 -0700

    removed default rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 DemoApp::Application.routes.draw do
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  root :to => 'pages#home'
+  
+  root_path => '/'
+  root_url  => 'http://localhost:3000/'
   get "pages/home"
 
   get "pages/contact"
