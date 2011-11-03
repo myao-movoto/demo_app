a4494db91ac47cba66e785a2b8dfcbf79913a0b5
MY <myao@movoto.com>
2011-11-03 14:43:47 -0700

    Layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 DemoApp::Application.routes.draw do
 
+  get "users/new"
+
+  match '/signup',  :to => 'users#new'
+  
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
   root :to => 'pages#home'
   
-  root_path => '/'
-  root_url  => 'http://localhost:3000/'
-  get "pages/home"
 
-  get "pages/contact"
-  
-  get "pages/about"
   
   resources :microposts
 
